42cdf60201c7223722f2f38d5bca68d68d3f8d6c
vaphi <vphi@uncc.edu>
2019-02-16 16:45:07 -0500

    about me section

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -1,17 +1,48 @@
 import React, {Component} from "react"
-
+import va from './Misc/va.png'
 class AboutMe extends Component {
     render() {
         return(
 
     <div className="Aboutme">
+            <div className="aboutmetitle">
+                <h1>About Me</h1>
+            </div>
           <div className="Aboutmepic">
-          <h1>Va M. Phi</h1>
-          <h6>Interested in Front-End | Back-End | Mobile Development </h6>
-            <p>|| UNC Charlotte Fall 2018 ||</p>
+          <img className="vaCont" src={va} alt="logo" />
+          <br/>
+          <br/>
+          <h1>Va Phi</h1>
          </div>
          <div className="aboutMeInfo">
-             <p>asd</p>
+            <div className="listCont">
+            <div className="skills">
+            <ul class="list-group-horizontal">
+            <li class="list-group-item list-group-item-primary">PROFICIENT :</li>
+            <li class="list-group-item list-group-item-success">Java</li>
+            <li class="list-group-item list-group-item-success">HTML/CSS</li>
+            <li class="list-group-item list-group-item-success">JavaScript</li>
+            <li class="list-group-item list-group-item-success">Sql</li>
+            </ul>
+            </div>
+            <div className="skills-1">
+            <ul class="list-group-horizontal">
+            <li class="list-group-item list-group-item-primary">FRAMEWORKS :</li>
+            <li class="list-group-item list-group-item-success">Android</li>
+            <li class="list-group-item list-group-item-success">React</li>
+            </ul>
+            </div>
+            <div className="skills-2">
+            <ul class="list-group-horizontal">
+            <li class="list-group-item list-group-item-primary">SOFTWARE TOOLS :</li>
+            <li class="list-group-item list-group-item-success">Android Studios</li>
+            <li class="list-group-item list-group-item-success">Git/Github</li>
+            <li class="list-group-item list-group-item-success">Visual Studios</li>
+            <li class="list-group-item list-group-item-success">MySQL</li>
+            <li class="list-group-item list-group-item-success">Eclipse</li>
+            </ul>
+            </div>
+            </div>
          </div>
     </div>
           
