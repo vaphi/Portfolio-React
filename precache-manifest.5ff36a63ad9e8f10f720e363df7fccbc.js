6ea9cc4c5706422872c42ca223367007b48b7049
vaphi <vphi@uncc.edu>
2019-06-01 18:15:55 -0400

    Updates

---
+++ precache-manifest.5ff36a63ad9e8f10f720e363df7fccbc.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "69a5a575023ea6ed0cd17ea5e97141e5",
+    "url": "/Home/static/media/emoji.69a5a575.png"
+  },
+  {
+    "revision": "1300d00b41ad57d42d9a",
+    "url": "/Home/static/css/main.65166590.chunk.css"
+  },
+  {
+    "revision": "16977f47057c83ffa1c4",
+    "url": "/Home/static/js/1.16977f47.chunk.js"
+  },
+  {
+    "revision": "2dfa8f231752af02153c",
+    "url": "/Home/static/js/runtime~main.2dfa8f23.js"
+  },
+  {
+    "revision": "1b5d769ab5705c74ff7ceaf3fb1a8a5d",
+    "url": "/Home/static/media/resume.1b5d769a.png"
+  },
+  {
+    "revision": "601b417205be72ecaa59c7b5f496df3d",
+    "url": "/Home/static/media/VaPhi_Resume.601b4172.pdf"
+  },
+  {
+    "revision": "bf7163ff69bb45ebca3d62476158573d",
+    "url": "/Home/static/media/va.bf7163ff.png"
+  },
+  {
+    "revision": "6f57e675ce8a8a9622473b63875ca922",
+    "url": "/Home/static/media/help.6f57e675.png"
+  },
+  {
+    "revision": "bdc4b7d303e7b8ef26b4b99063c99498",
+    "url": "/Home/static/media/weather.bdc4b7d3.png"
+  },
+  {
+    "revision": "1300d00b41ad57d42d9a",
+    "url": "/Home/static/js/main.1300d00b.chunk.js"
+  },
+  {
+    "revision": "0c04fb6149c39fb7c5c3e9afe5e7be9e",
+    "url": "/Home/static/media/dadjokes.0c04fb61.png"
+  },
+  {
+    "revision": "0bbeb89633ac11bbfc8937e95488b5e1",
+    "url": "/Home/static/media/meme.0bbeb896.png"
+  },
+  {
+    "revision": "ec9411875ac8d9a19f039b42ea10ed12",
+    "url": "/Home/static/media/login.ec941187.png"
+  },
+  {
+    "revision": "f6e0b7f4c3191bbb9d7d15ddc55d7957",
+    "url": "/Home/static/media/signup.f6e0b7f4.png"
+  },
+  {
+    "revision": "d593875dae8ce0821a3496849bc15689",
+    "url": "/Home/static/media/payment.d593875d.png"
+  },
+  {
+    "revision": "785de59ee670f1a25ea86c501a2c6c91",
+    "url": "/Home/static/media/token.785de59e.png"
+  },
+  {
+    "revision": "717f55b10a7748fb99734a3a3f22e514",
+    "url": "/Home/static/media/ty.717f55b1.png"
+  },
+  {
+    "revision": "baf84802ca66f68a13309caa37af32c4",
+    "url": "/Home/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Portfolio-React/precache-manifest.2043b8a454759a673bd1ff97144cc657.js"
+  "/Home/precache-manifest.5ff36a63ad9e8f10f720e363df7fccbc.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/Portfolio-React/index.html", {
+workbox.routing.registerNavigationRoute("/Home/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
